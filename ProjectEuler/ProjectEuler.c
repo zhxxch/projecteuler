f0f148f290b453a8925290aa3c30d2063c5eba00
ZHANG X <zhxxch@outlook.com>
2017-12-16 22:45:51 +0800

    rdrand64

--- ProjectEuler/ProjectEuler.c
+++ ProjectEuler/ProjectEuler.c
@@ -6,12 +6,9 @@
 #include"Header.h"
 const int MB = 1024 * 1024;
 int main(int argc, char *argv[]){
-    void* WorkingMem = malloc(8 * MB);
-    const int ElemNum = 8 * MB / sizeof(unsigned long long);
+    void* WorkingMem = malloc(1024 * MB);
+    const int ElemNum = 1024 * MB / sizeof(unsigned long long);
     long long dt = p213_simul(WorkingMem, ElemNum);
-    FILE *fd = fopen("speck10", "wb+");
-    fwrite(WorkingMem, sizeof(unsigned long long), ElemNum, fd);
-    fclose(fd);
-    printf("dt = %lli\n", dt);
+    printf("dt = %lli ms\n", dt);
     return 0;
 }
--- ProjectEuler/Source.cpp
+++ ProjectEuler/Source.cpp
@@ -6,24 +6,21 @@
 #include<random>
 #include<chrono>
 #include<zxcrypto.h>
+#include<zxnoise.h>
+
 
 using heure = std::chrono::high_resolution_clock;
 using ms = std::chrono::milliseconds;
 long long p213_simul(unsigned long long *Mem, const int Num){
     uint64_t Key[2] = {0x0706050403020100ull, 0x0f0e0d0c0b0a0908ull};
     uint64_t Pt[2] = {0x7469206564616d20ull, 0x6c61766975716520ull};
-    //Mem[0] = 0x74614620736e6165ull;
-    //Mem[1] = 0x6c61766975716520ull;
     uint64_t RoundKeys[32];
     heure::time_point t0 = heure::now();
-    //speckNx4x128u128(Mem, Mem, RoundKeys, Num);
+    unsigned long long r64;
     for(int i = 0; i < Num; i++){
-        Mem[i] = speck64u96(i, 0x03020100u, 0x0b0a0908u, 0x13121110u);
-        //speck4x128u128(Mem + i, Mem + i, RoundKeys);
+        r64 = ru64_rdrand();
     }
-    
+    Mem[0] = r64;
     heure::time_point t1 = heure::now();
-    printf("Ct: %llx %llx\n", Mem[0], Mem[1]);
-    printf("Ct = %llx\n", speck64u96(0x74614620736e6165ull, 0x03020100u, 0x0b0a0908u, 0x13121110u));
     return std::chrono::duration_cast<ms>(t1 - t0).count();
 }
